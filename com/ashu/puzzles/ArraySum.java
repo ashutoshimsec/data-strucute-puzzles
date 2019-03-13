c8b447b2c2af118fac0788328b4b721db1fdb1cf
ashutosh Kumar <ashutoshimsec@gmail.com>
2019-03-13 22:47:58 +0530

    inital commit

--- com/ashu/puzzles/ArraySum.java
+++ com/ashu/puzzles/ArraySum.java
@@ -1,7 +1,16 @@
+package ashu.puzzles;
+
 public class ArraySum {
 
 
     public static void main(String[] args) {
         System.out.println("comming");
+    }
+
+
+    private static String sumArray(int sumValue, int[] inputArray) {
+
+
+        return null;
     }
 }
